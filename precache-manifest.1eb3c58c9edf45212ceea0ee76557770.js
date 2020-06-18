01d518f5a488e205df7dcb7b7a5d0ba6dba39287
masun714 <masun714@gmail.com>
2020-06-18 18:41:34 +0800

    Updates

---
+++ precache-manifest.1eb3c58c9edf45212ceea0ee76557770.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f45993fcd2b0eb84e0e5bbfedbba8834",
+    "url": "/m/index.html"
+  },
+  {
+    "revision": "487c09a5272138b51b8d",
+    "url": "/m/static/css/main.08121c6d.chunk.css"
+  },
+  {
+    "revision": "a118fcff70fde5bdac91",
+    "url": "/m/static/js/2.7bede9e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/m/static/js/2.7bede9e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "487c09a5272138b51b8d",
+    "url": "/m/static/js/main.04f1b843.chunk.js"
+  },
+  {
+    "revision": "5d75453bde9f444e5ad0",
+    "url": "/m/static/js/runtime-main.5004c9f3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/m/static/media/logo.5d5d9eef.svg"
+  }
+]);
